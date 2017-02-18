d5bf42d82bf8f5249d70674c22a254ce43233cf2
defuera <vdenis@zvooq.com>
2017-02-18 18:01:55 +0100

    in progress

--- Application/src/main/java/com/example/android/camera2basic/CameraActivity.kt
+++ Application/src/main/java/com/example/android/camera2basic/CameraActivity.kt
@@ -17,19 +17,29 @@
 package com.example.android.camera2basic
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import java.io.File
+
 
 class CameraActivity : Activity() {
 
-    private var uiLayer: ImageView? = null
-    private var animationLayer: ImageView? = null
+    private lateinit var uiLayer: ImageView
+    private lateinit var animationLayer: ImageView
+    private lateinit var prefs: SharedPreferences
+
+    val PATH_DIRECTORY = "/sdcard/cfaker/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        prefs = getSharedPreferences("sp", Context.MODE_PRIVATE)
 
         if (null == savedInstanceState) {
             fragmentManager.beginTransaction()
@@ -41,10 +51,57 @@ class CameraActivity : Activity() {
         animationLayer = findViewById(R.id.animation_layer) as ImageView
 
 
-        uiLayer!!.setOnLongClickListener {
-            Toast.makeText(this@CameraActivity, "long press", Toast.LENGTH_SHORT).show()
+        uiLayer.setOnLongClickListener {
+            showInputDirectoryNameDialog()
             false
         }
+
+        uiLayer.setOnClickListener {
+            loadPicturesAndAnimate()
+        }
+    }
+
+    private fun showInputDirectoryNameDialog() {
+        val editText = EditText(this)
+        val padding = resources.getDimensionPixelOffset(R.dimen.padding_16)
+        editText.setPadding(padding, padding, padding, padding)
+        val savedPath = getSavedDirectory()
+
+        editText.setText(savedPath)
+        editText.setSelection(savedPath.length)
+
+
+        AlertDialog.Builder(this)
+                .setTitle("Укажите путь к папке с ui.png и файлами анимации. ")
+                .setMessage("Папка должна быть помещена на внутренню память телефона по адреусу $PATH_DIRECTORY и не иметь в названии пробелов")
+                .setView(editText)
+                .setPositiveButton(
+                        "OK",
+                        { dialogInterface, i ->
+                            dialogInterface.dismiss()
+                            saveDirectory(editText.text.toString())
+                        }
+                )
+                .show()
+    }
+
+    private fun loadPicturesAndAnimate() {
+        val dirName = getSavedDirectory()
+        val dir = File(dirName)
+        if (!dir.exists()){
+            Toast.makeText(this, "Директории с названием $dirName не существует, проверьте правильность ввода данных.", Toast.LENGTH_SHORT).show()
+            showInputDirectoryNameDialog()
+        } else {
+//            create
+        }
+    }
+
+    private fun getSavedDirectory(): String {
+        return prefs.getString("path", PATH_DIRECTORY)
+    }
+
+    private fun saveDirectory(path: String) {
+        prefs.edit().putString("path", path).apply()
     }
 
 }
